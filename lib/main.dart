007fd217c6649b7e13137b3302b5a456041476de
osvalucero <oslu_20@alu.uabcs.mx>
2023-09-18 10:18:08 -0700

    validacion_campos_2

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Login and Signup',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       initialRoute: '/',
       routes: {
